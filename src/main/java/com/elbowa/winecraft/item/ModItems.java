ed2daa2323c71a138320522ee9311176fe947ca9
ElBowa <edoardo.bertolinoicloud.com>
2022-01-05 14:19:38 +0100

    Red Grape implemented

--- src/main/java/com/elbowa/winecraft/item/ModItems.java
+++ src/main/java/com/elbowa/winecraft/item/ModItems.java
@@ -20,20 +20,36 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, WineCraft.MOD_ID);
 
+//Registra la bottiglia vuota
 public static final RegistryObject<Item> EM_BOT = ITEMS.register("em_bot",
         ()-> new Item(new Item.Properties()
-                .tab(CreativeModeTab.TAB_BREWING)
+                .tab(ModCreativeModeTab.WINECRAFT_TAB)
                 .rarity(Rarity.COMMON)
                 .stacksTo(4)));
 
-    public static final RegistryObject<Item> WINE_BOT = ITEMS.register("wine_bot",
-            ()-> new Item(new Item.Properties().rarity(Rarity.COMMON).stacksTo(4).tab(CreativeModeTab.TAB_FOOD).food(
-                    new FoodProperties.Builder()
-                            .effect(()-> new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN,70,2), 100)
-                            .effect(()-> new MobEffectInstance(MobEffects.CONFUSION,50,3), 100)
-                            .saturationMod(0.3f)
-                            .nutrition(0)
-            .build())));
+//Registra il grappolo d'uva rossa
+public static final RegistryObject<Item> RED_GRAPE = ITEMS.register("red_grape",
+        ()-> new Item((new Item.Properties()
+        .tab(ModCreativeModeTab.WINECRAFT_TAB)
+        .stacksTo(64)
+        .rarity(Rarity.COMMON)
+        .food(new FoodProperties.Builder()
+                .nutrition(1)
+                .saturationMod(0.5f)
+                .fast()
+                .build()))));
+
+//Registra la bottiglia di vino rosso
+public static final RegistryObject<Item> WINE_BOT = ITEMS.register("wine_bot",
+        ()-> new Item(new Item.Properties().rarity(Rarity.COMMON).stacksTo(4).tab(ModCreativeModeTab.WINECRAFT_TAB).food(
+                new FoodProperties.Builder()
+                        .effect(()-> new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN,70,2), 100)
+                        .effect(()-> new MobEffectInstance(MobEffects.CONFUSION,50,3), 100)
+                        .saturationMod(0.3f)
+                        .nutrition(0)
+                        .build())));
+
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
